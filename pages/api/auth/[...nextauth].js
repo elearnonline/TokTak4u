074834b507633286a605c3981265ef7b7ca0d7d6
James Eco <elearnnow1@gmail.com>
2022-10-17 15:04:09 +0300

    first commit+ novu

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,103 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import GithubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitter";
+import bcrypt from "bcrypt";
+import { prisma } from "../../../prisma/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import EmailProvider from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options = {
+  site: process.env.NEXTAUTH_URL,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+
+      maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID_SECRET,
+      clientSecret: process.env.TWITTER_SECRET_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req, res) {
+        try {
+          const user = await prisma.user.findUnique({
+            where: { email: credentials.email },
+          });
+
+          if (!user) throw new Error("No user found");
+
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isPasswordValid) throw new Error("Password is not valid");
+
+          return {
+            email: user.email,
+            id: user.id,
+            name: user.name,
+            isAdmin: user.isAdmin,
+            roles: user.roles,
+          };
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  secret: "yudjXHbqE5VH4LkwZ4srgsdL2EZrjp",
+
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
+  database: process.env.DATABASE_URL,
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+    "verify-request": "/auth/verify-request",
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      user && (token.user = user);
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token?.accessToken;
+      session.id = token?.user.id;
+      session.user.isAdmin = token?.user.isAdmin;
+      session.user.id = token?.user.id;
+      session.roles = token?.user.roles;
+
+      return session;
+    },
+  },
+  debug: true,
+};
+
+export default (req, res) => NextAuth(req, res, options);
